454ea3daafc524559eff7e172b2d98bcc5311a20
Keshav <keshavarora0114@gmail.com>
2022-06-18 14:12:40 +0530

    Add Backend API

--- src/components/rightbar/Rightbar.js
+++ src/components/rightbar/Rightbar.js
@@ -16,7 +16,9 @@ export default function Rightbar({ user }) {
   useEffect(() => {
     const getFriends = async () => {
       try {
-        const friendList = await axios.get("/users/friends/" + user._id);
+        const friendList = await axios.get(
+          "https://hello-social-app.herokuapp.com/api/users/friends/" + user._id
+        );
         setFriends(friendList.data);
       } catch (err) {}
     };
@@ -51,14 +53,20 @@ export default function Rightbar({ user }) {
     const handleClick = async () => {
       try {
         if (followed) {
-          await axios.put(`/users/${user._id}/unfollow`, {
-            userId: currentUser._id,
-          });
+          await axios.put(
+            `https://hello-social-app.herokuapp.com/api/users/${user._id}/unfollow`,
+            {
+              userId: currentUser._id,
+            }
+          );
           dispatch({ type: "UNFOLLOW", payload: user._id });
         } else {
-          await axios.put(`/users/${user._id}/follow`, {
-            userId: currentUser._id,
-          });
+          await axios.put(
+            `https://hello-social-app.herokuapp.com/api/users/${user._id}/follow`,
+            {
+              userId: currentUser._id,
+            }
+          );
           dispatch({ type: "FOLLOW", payload: user._id });
         }
         setFollowed(!followed);
